ca956e0a03631cf51ffa138610a87a3cc6e4c3d3
devsai9 <63507117+devsai9@users.noreply.github.com>
2026-04-12 13:57:32 -0400

    Update lab name and homepage

--- components/load-nav-footer.js
+++ components/load-nav-footer.js
@@ -1,11 +1,14 @@
-import { init } from "./mobile-nav-toggle.js";
+import { initMobileNavListeners, handleNavOnScroll } from "./nav-scripts.js";
 
 fetch("components/nav.html")
     .then(res => res.text())
     .then(data => {
-        document.querySelector("#nav-insert").innerHTML = data
+        const navIns = document.querySelector("#nav-insert");
+        navIns.parentElement.innerHTML = data + navIns.parentElement.innerHTML;
+        navIns.remove();
     })
-    .then(init);
+    .then(initMobileNavListeners)
+    .then(handleNavOnScroll);
 
 fetch("components/footer.html")
     .then(res => res.text())
